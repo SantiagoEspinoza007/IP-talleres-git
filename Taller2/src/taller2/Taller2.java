1f529b80e77df91e8fec4d1b07a54f9c62f46a05
Edgar Santiago <esantiagoespinoza08@gmail.com>
2021-10-24 22:23:49 -0500

    Subiendo archivos iniciales de java y netbeans

---
+++ Taller2/src/taller2/Taller2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller2;
+
+/**
+ *
+ * @author Santiago Espinoza
+ */
+public class Taller2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo con git esto es un demo del uso de GitHub y NetBeans");
+        
+    }
+    
+}
